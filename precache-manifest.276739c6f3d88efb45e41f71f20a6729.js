730872b8c73c7927f9400d51b1b354311a3b805e
Maor Bar <maor44@gmail.com>
2019-02-11 23:47:28 +0200

    Updates

---
+++ precache-manifest.276739c6f3d88efb45e41f71f20a6729.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9952bc976026d19fe3c1",
+    "url": "/netflux/static/js/runtime~main.9952bc97.js"
+  },
+  {
+    "revision": "d855dc02224bad326c28",
+    "url": "/netflux/static/js/main.d855dc02.chunk.js"
+  },
+  {
+    "revision": "3ef4cda0dad79c9e4dd6",
+    "url": "/netflux/static/js/1.3ef4cda0.chunk.js"
+  },
+  {
+    "revision": "d855dc02224bad326c28",
+    "url": "/netflux/static/css/main.1f5b893e.chunk.css"
+  },
+  {
+    "revision": "bf284e89e818d7aa58d2c93789a8a75b",
+    "url": "/netflux/index.html"
+  }
+];
